b93fad11c69744df7bdb23aaa2ef095bd6cb97ff
Mateus Santos <mateuso12@hotmail.com.br>
2025-11-07 17:08:36 -0300

    refactor: update Prettier configuration and remove unused ESLint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -18,9 +17,7 @@ export default defineConfig([
       reactRefresh.configs.vite,
       prettierConfig,
     ],
-    plugins: {
-      prettier,
-    },
+    plugins: {},
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -34,7 +31,6 @@ export default defineConfig([
       },
     },
     rules: {
-      'prettier/prettier': 'error',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
